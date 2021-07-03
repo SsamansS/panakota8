77945e6af91ea21dc46c313dfd11e96fa54744f2
SsamansS <nurlanov.aman@bk.ru>
2021-07-03 16:14:33 +0600

    Написал умную выборку по запросу пользователя. Теперь нужно прост интегрировать в тг

--- panakota8/Program.cs
+++ panakota8/Program.cs
@@ -22,13 +22,87 @@ namespace panakota8
     {
         static void Main(string[] args)
         {
-            try
+            //try
+            //{
+            //    TelegramBotic hlp = new TelegramBotic(token: "сюда нужно вставить токен вашего телеграм бота");
+            //    hlp.GetUppdates();
+            //}
+
+            //catch (Exception ex) { Console.WriteLine(ex.Message); }   
+            //Console.WriteLine("Бек д.д.");
+            //foreach (char letter in GetCapitalLetters("Бек д.д."))
+            //    Console.Write(letter);
+
+            //Console.WriteLine(new string('-', 50));
+
+            //Console.WriteLine("Байбакпаев Э.Д.");
+            //foreach (char letter in GetCapitalLetters("Байбакпаев Э.Д."))
+            //{
+            //    Console.Write(letter);
+            //}
+            //Console.WriteLine(GetCapitalLetters("Байбакпаев Э.Д.").Contains(GetCapitalLetters("Бек д.д.")[0]));
+            //Console.WriteLine(GetCapitalLetters("Байбакпаев Э.Д.").Contains(GetCapitalLetters("Бек д.д.")[1]));
+            //Console.WriteLine(GetCapitalLetters("Байбакпаев Э.Д.").Contains(GetCapitalLetters("Бек д.д.")[2]));
+
+            //Console.WriteLine(new string('-', 50));
+            //Console.WriteLine(GetCapitalLetters("Бек д.д.").Contains(GetCapitalLetters("Байбакпаев Э.Д.")[0]));
+            //Console.WriteLine(GetCapitalLetters("Бек д.д.").Contains(GetCapitalLetters("Байбакпаев Э.Д.")[1]));
+            //Console.WriteLine(GetCapitalLetters("Бек д.д.").Contains(GetCapitalLetters("Байбакпаев Э.Д.")[2]));
+            //Console.WriteLine(new string('-', 50));
+
+            //Console.WriteLine("Аттокуров Д.Б.");
+            //foreach (char letter in GetCapitalLetters("Аттокуров Д.Б."))
+            //    Console.Write(letter);
+
+            //Console.WriteLine(new string('-', 50));
+
+            //Console.WriteLine("Бакчиев Д.А.");
+            //foreach (char letter in GetCapitalLetters("Бакчиев Д.А."))
+            //    Console.Write(letter);
+
+            //Console.WriteLine(new string('-', 50));
+
+            //GetCombination();
+
+            //Console.WriteLine(new string('-', 50));
+
+            processingDatasFormApi processing = new processingDatasFormApi();
+            foreach (string dep in processing.GetCombinations("Токтошев Э.Т."))
+                Console.WriteLine(dep);
+
+            //foreach (char dep in GetCapitalLetters("Бек д.д."))
+            //    Console.WriteLine(dep);
+        }
+        static void  GetCombination()
+        {
+            ConectWithAPI re = new ConectWithAPI();
+            List<Deputy> Deputies = re.getResponseOfDeputies();
+
+            List<string> list = new List<string>();
+
+            foreach (Deputy deputy in Deputies)
             {
-                TelegramBotic hlp = new TelegramBotic(token: "сюда нужно вставить токен вашего телеграм бота");
-                hlp.GetUppdates();
+                //int Curr = 0;
+                List<char> CapitalLettersAPI = GetCapitalLetters(deputy.Name);
+                foreach(char letter in CapitalLettersAPI)
+                {
+                    Console.Write(letter + ",");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+        public static List<char> GetCapitalLetters(string MBdep)
+        {
+            List<char> response = new List<char>();
+
+            string[] ArrOfNam = MBdep.Split(new char[] { ' ', '.' });
+            foreach (string word in ArrOfNam)
+            {
+                if (word != null && word != " " && word != "")
+                    response.Add(word.ToLower()[0]);
             }
 
-            catch (Exception ex) { Console.WriteLine(ex.Message); }   
-        }    
+            return response;
+        }
     }
 }
--- panakota8/processing/processingDatasFormApi.cs
+++ panakota8/processing/processingDatasFormApi.cs
@@ -6,7 +6,9 @@ namespace panakota8.processing
 {
     class processingDatasFormApi
     {
-        ConectWithAPI re = new ConectWithAPI();
+        static ConectWithAPI re = new ConectWithAPI();
+        List<Deputy> Deputies = re.getResponseOfDeputies();
+
         public string TheDecision(string name, Decision decision)
         {
             string answer = "";
@@ -26,9 +28,7 @@ namespace panakota8.processing
 
         public string CheckDeputy(string text)
         {
-            ConectWithAPI depities = new ConectWithAPI();
-
-            foreach (Deputy item in depities.getResponseOfDeputies())
+            foreach (Deputy item in Deputies)
             {
                 if (item.Name.ToLower() == text.ToLower())
                 {
@@ -38,18 +38,83 @@ namespace panakota8.processing
             return "f";
         }
 
-        /*public Decision CheckDecision(string status)
+
+
+
+
+
+        public List<string> GetCombinations(string MBdeputy)
         {
-            if (status.ToLower() == "отсутствовал")
-                return Decision.Absent;
-            else if (status.ToLower() == "за")
-                return Decision.Agreed;
-            else if (status.ToLower() == "инициатор")
-                return Decision.Initiator;
-            else if (status.ToLower() == "против")
-                return Decision.Rejected;
+            List<string> deputies = new List<string>();
+
+            List<char> CapitalLetters = GetCapitalLetters(MBdeputy);
+            deputies.AddRange(GetCombination(CapitalLetters));
+
+            return deputies;
+        }
+        List<string> GetCombination(List<char> CapitalLetters)
+        {
+            List<string> listEvery = new List<string>();
+            List<string> listEveryBackwards = new List<string>();
+
+            foreach (Deputy deputy in Deputies)
+            {
+                string Every = Switcher(true, deputy, CapitalLetters);
+                if(Every != "")
+                    listEvery.Add(Every);
+
+                string EveryBackwards = Switcher(false, deputy, CapitalLetters);
+                if(EveryBackwards != "")
+                    listEveryBackwards.Add(EveryBackwards);
+
+            }
+            if(listEvery.Count < listEveryBackwards.Count) { return listEvery; }
+            return listEveryBackwards;
+        }
+
+        private string Switcher(bool switcher, Deputy deputy, List<char> capitalLetters)
+        {
+            List<char> CapitalLettersAPI = GetCapitalLetters(deputy.Name);
+
+            string list = "";
+
+            if(switcher == true)
+            {
+                int Curr = 0;
+                for (int i = 0; i < capitalLetters.Count; i++)
+                {
+                    if (CapitalLettersAPI.Contains(capitalLetters[i]))
+                        ++Curr;
+                }
+                if (Curr == 3)
+                    list = deputy.Name;
+            }
             else
-                return Decision.Nothing;
-        }*/
+            {
+                int Curr = 0;
+                for (int i = 0; i < CapitalLettersAPI.Count; i++)
+                {
+                    if (capitalLetters.Contains(CapitalLettersAPI[i]))
+                        ++Curr;
+                }
+                if (Curr == 3)
+                    list = deputy.Name;
+            }
+            return list;
+        }
+
+        List<char> GetCapitalLetters(string MBdep)
+        {
+            List<char> response = new List<char>(3);
+
+            string[] ArrOfNam = MBdep.Split(new char[] { ' ', '.' });
+            foreach(string word in ArrOfNam)
+            {
+                if (word != null && word != " " && word != "")
+                    response.Add(word.ToLower()[0]);
+            }
+
+            return response;
+        }
     }
 }
